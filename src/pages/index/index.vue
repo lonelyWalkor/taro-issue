7584239679f69ccdbf06f40973d9c9aba433f105
章洪 <zhanghong@zhanghongdeMacBook-Pro.local>
2022-12-07 15:01:26 +0800

    issuw

---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="index" style="filter: grayscale(1)">
+    <!-- 上方这个style不会最终输出 -->
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import './index.css'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
